a0572a1bc80a519616b630f44fb1c2380252f191
Ved Dangat <veddangat123@gmail.com>
2026-07-30 22:11:57 +0530

    cjc code

---
+++ Calculator.java
@@ -0,0 +1,22 @@
+public class Calculator{
+
+	public void addition(){
+	System.out.println(10+10);
+	}
+
+	public void subtraction(){
+	System.out.println(10-3);
+	}
+	
+	public void multiplication(){
+	System.out.println(10*10);
+	}
+
+	public void division(){
+	System.out.println(100/10);
+	}
+
+	public void modlous(){
+	System.out.println(100%10);
+	}
+}
